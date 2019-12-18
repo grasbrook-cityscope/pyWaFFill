0bd36fa0edd1ecc33dffc68798714f5029738495
andredaa <post.for.andre@gmail.com>
2019-12-18 13:41:52 +0100

    Tag container names with instances ids to allow initialization of multiple instances

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-docker stop gracio_pywaffill_instance
-docker rm gracio_pywaffill_instance
 if [ "$#" -gt 0 ]; then # if command line arguments were given
-    docker run --name gracio_pywaffill_instance -d gracio_pywaffill --endpoint $1
+    docker stop gracio_pywaffill_instance_$1
+    docker rm gracio_pywaffill_instance_$1
+    docker run --name gracio_pywaffill_instance_$1 -d gracio_pywaffill --endpoint $1
+    # docker logs -f gracio_pywaffill_instance_$1  ## do not force logs when multiple instances start
+
 else # no command line args -> don't choose endpoint
-    docker run --name gracio_pywaffill_instance -d gracio_pywaffill
+    docker stop gracio_pywaffill
+    docker rm gracio_pywaffill
+    docker run --name gracio_pywaffill -d gracio_pywaffill
+    docker logs -f gracio_pywaffill
 fi
-docker logs -f gracio_pywaffill_instance
