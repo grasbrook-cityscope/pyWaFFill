2de4540331866713997a000252ce108afa8f62e2
Jonas <jonas.luft@hcu-hamburg.de>
2020-02-12 15:45:07 +0100

    provide walkspeed to run.sh/docker

--- run.sh
+++ run.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
 if [ "$#" -gt 0 ]; then # if command line arguments were given
-    docker stop gracio_pywaffill_instance_$1
-    docker rm gracio_pywaffill_instance_$1
-    docker run --name gracio_pywaffill_instance_$1 -d gracio_pywaffill --endpoint $1
+    if [ "$#" -gt 1 ]; then # spped argument given
+        echo "walking speed provided"
+        docker stop gracio_pywaffill_instance_$1_$2
+        docker rm gracio_pywaffill_instance_$1_$2
+        docker create --name gracio_pywaffill_instance_$1_$2 gracio_pywaffill --endpoint $1
+        docker cp config_$2.json gracio_pywaffill_instance_$1_$2:/app/config.json
+        docker start gracio_pywaffill_instance_$1_$2
+    else
+        docker stop gracio_pywaffill_instance_$1
+        docker rm gracio_pywaffill_instance_$1
+        docker run --name gracio_pywaffill_instance_$1 -d gracio_pywaffill --endpoint $1
+    fi
+    
     # docker logs -f gracio_pywaffill_instance_$1  ## do not force logs when multiple instances start
 
 else # no command line args -> don't choose endpoint
